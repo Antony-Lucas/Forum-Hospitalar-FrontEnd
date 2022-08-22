900dd5eedab41f891e4ae6c7cc92d4c38f1653b2
antony-lucas <lawokahama@gmail.com>
2022-08-22 00:40:31 -0300

    answers addes

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -8,8 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.demo.entities.Answers;
 import com.example.demo.entities.Asks;
 import com.example.demo.entities.User;
+import com.example.demo.repositories.AnswersRepositories;
 import com.example.demo.repositories.AsksRepositories;
 import com.example.demo.repositories.UserRepositories;
 
@@ -21,6 +23,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private AsksRepositories askRepository;
+	
+	@Autowired
+	private AnswersRepositories answerRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -31,8 +36,14 @@ public class TestConfig implements CommandLineRunner{
 		Asks o2 = new Asks(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
 		Asks o3 = new Asks(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
 		
+		Answers cat1 = new Answers(null, Instant.parse("2019-06-20T19:53:07Z"), "this.content.return1", o1);
+		Answers cat2 = new Answers(null, Instant.parse("2019-07-21T03:42:10Z"), "this.content.return2", o2);
+		Answers cat3 = new Answers(null, Instant.parse("2019-07-21T03:42:10Z"), "this.content.return3", o3); 
+
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		askRepository.saveAll(Arrays.asList(o1,o2,o3));
+		answerRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 	}
 
 }
