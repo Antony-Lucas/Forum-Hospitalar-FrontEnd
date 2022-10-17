6f131e0ddc7e7a85abc41fe1fcb4bec73425184c
antony-lucas <lawokahama@gmail.com>
2022-10-16 23:58:28 -0300

    fixing bugs on signUp

--- javaScript/API/signUp/signUp.js
+++ javaScript/API/signUp/signUp.js
@@ -1,5 +1,5 @@
 import  {url_api}  from "../config.js";
-import { email, links, pass, submit, user } from "./signUpErrors.js";
+import { email, error_auth, error_empty_field, links, load_circle, pass, submit, user, userName } from "./signUpErrors.js";
 
 submit.addEventListener('click', async function(){
     try {
@@ -11,6 +11,7 @@ submit.addEventListener('click', async function(){
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
+                userName: userName.value,
                 name: user.value,
                 email: email.value,
                 password: pass.value
@@ -18,19 +19,31 @@ submit.addEventListener('click', async function(){
         })
         .then(
             response => {
-                if(user.value == "" || pass.value == ""){
-                    error_visible.style.visibility = "visible"
+                if(response.status == 201 || response.status == 200){
+                   
+                    return response.json();
+                }
+                if(userName == "" || user.value == "" || email.value == "" || pass.value.length == ""){
+                    error_empty_field.style.visibility = "visible"
                     load_circle.style.visibility = 'hidden'
                     return false;
                 }
                 else
                 {
-                    response.json();
+                    return response.json();
                 }
             }
         )
         .then(
-            data => console.log(data)
+            data => {
+                if(data.status == 400){
+                    error_empty_field.style.visibility = "visible";
+                    load_circle.style.visibility = 'hidden'
+                    links.style.top = "35px";
+                    error_auth.innerHTML = data.message;
+                }
+                return console.log(data.message);
+            }
         )
     } catch (error) {
         console.log(error);
--- javaScript/API/signUp/signUpErrors.js
+++ javaScript/API/signUp/signUpErrors.js
@@ -1,3 +1,4 @@
+export var userName = document.getElementById('newUserNameFull');
 export var user = document.getElementById('newUser');
 export var email = document.getElementById('newEmail');
 export var pass = document.getElementById('newPassword');
@@ -8,7 +9,7 @@ export var load_circle = document.getElementById('loader');
 export var links = document.getElementById('links');
 
 submit.onclick = function(){
-    if(user.value.length == 0 || email.value.length == 0 || pass.value.length == 0){
+    if(userName.value.length == 0 || user.value.length == 0 || email.value.length == 0 || pass.value.length == 0){
         error_empty_field.style.visibility = "visible";
         links.style.top = "35px";
         return false;
