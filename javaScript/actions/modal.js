199f45c3fb416da08407b46123f43818e37adb63
antony-lucas <lawokahama@gmail.com>
2022-10-20 23:41:02 -0300

    ready

--- javaScript/actions/modal.js
+++ javaScript/actions/modal.js
@@ -1,17 +1,42 @@
+var mainMenu = document.getElementById("mainMenu");
+var mainModal = document.getElementById("myMainModal");
 var modal = document.getElementById("myModal");
-var button = document.getElementById("showModal");
-var span = document.getElementsByClassName("close")[0];
+var modalAttendance = document.getElementById("myModalAttendance");
 
-button.onclick = function(){
+var closeModalAtt = document.getElementById("closeModalAttendance");
+
+function openMainModal(){
+    mainModal.style.display = "block";
+}
+
+function closeMainModal(){
+    mainModal.style.display = "none";
+}
+
+function openModal(){
     modal.style.display = "block";
 }
 
-span.onclick = function(){
+function closeModal(){
     modal.style.display = "none";
+}
+
+function openModalAttendance(){
+    modalAttendance.style.display = "block";
+    mainMenu.style.display = "block"
+}
+
+function closeModalAttendance(){
+    modalAttendance.style.display = "none";
+    mainMenu.style.display = "none";
 }
 
 window.onclick = function(event){
     if(event.target == modal){
         modal.style.display = "none";
+    }
+
+    if(event.target == mainModal){
+        mainModal.style.display = "none"
     }
 }
