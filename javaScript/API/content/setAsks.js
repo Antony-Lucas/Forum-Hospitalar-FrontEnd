83ba5f522fac81182d0a92d3dc2e2b14d45e27e1
antony-lucas <lawokahama@gmail.com>
2022-11-13 23:30:44 -0300

    set image

--- javaScript/API/content/setAsks.js
+++ javaScript/API/content/setAsks.js
@@ -8,9 +8,32 @@ var submit_ask = document.getElementById("submitAsk");
 var user_id = localStorage.getItem("id_session");
 var management_id = localStorage.getItem("management_session");
 var getTime = date_.toLocaleString();
+var setImage = document.getElementById("image-button");
 var setDep = 1;
 
 submit_ask.addEventListener("click", async function(){
+    const formData = new FormData();
+    formData.append('file', setImage.files[0]);
+    formData.append('url', 'filename');
+    try {
+        await fetch("http://localhost:8080/upload",{
+            method: "POST",
+            headers: {
+                "Authorization":"Bearer " + bearer
+            },
+            body: formData
+        }).then(function(response) {
+            return response.text();
+        })
+        .then(function(data) {
+            var imageUrl = data;
+            console.log(imageUrl);
+            return imageUrl;
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
     var set_ask = document.getElementById("askContent").value;
     try{
         await fetch(url_api + "/modules/departments/asks", {
