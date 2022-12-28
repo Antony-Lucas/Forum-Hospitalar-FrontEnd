e15db60fc23d883f4224ca9d7f7f6b372cbbbbdd
antony-lucas <lawokahama@gmail.com>
2022-12-28 11:40:29 -0300

    att

--- src/main/java/com/example/demo/repositories/AwsRepositories.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.example.demo.entities.Aws;
-@Repository
-public interface AwsRepositories extends CrudRepository<Aws, Integer>{
-	
-}
--- src/main/java/com/example/demo/services/exception/DataBaseException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.services.exception;
-
-public class DataBaseException extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-	
-	public DataBaseException(String msg) {
-		super(msg);
-	}
-}
